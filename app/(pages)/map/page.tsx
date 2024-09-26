706b8883cdbcd48e33384bb4f4fad5330c30889f
euger <azkriven16@gmail.com>
2024-09-26 21:54:23 +0800

    render markers map

--- app/(pages)/map/page.tsx
+++ app/(pages)/map/page.tsx
@@ -1,49 +1,47 @@
 "use client";
 import Section from "@/components/common/section";
-import { stores } from "@/lib/dummydata";
 import mapboxgl from "mapbox-gl";
-import { Suspense, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useSearchParams } from "next/navigation";
 import MapSheet from "@/components/MapSheet";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import { useTheme } from "next-themes";
+import { usePosts } from "@/lib/hooks/usePosts";
 
 export default function Home() {
     const searchParams = useSearchParams();
+    const { theme } = useTheme();
     const mapContainer = useRef<any>(null);
     const map = useRef<mapboxgl.Map | any>(null);
-    const mapStyle = searchParams.get("map_styles");
-    console.log(mapStyle);
+    const mapId = searchParams.get("id");
 
     const TOKEN =
         "pk.eyJ1IjoiYXprcml2ZW4xNiIsImEiOiJjbGhma3IxaWcxN3c3M2VyM3VhcGsxcHk3In0.pto_0eshW9NHMP-m1O_blg";
 
+    const { data, isPending, isFetching } = usePosts(100);
+
     useEffect(() => {
         mapboxgl.accessToken = TOKEN;
 
+        // Determine the initial map style based on theme and mapId
+        const initialMapStyle = mapId
+            ? `mapbox://styles/mapbox/${mapId}`
+            : theme === "dark"
+            ? "mapbox://styles/mapbox/dark-v11"
+            : "mapbox://styles/mapbox/light-v11";
+
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: mapStyle || "mapbox://styles/mapbox/streets-v12",
-            center: [-77.034084, 38.909671],
+            style: initialMapStyle,
+            center: [123.14389088712784, 11.461424460015792],
             zoom: 13,
             scrollZoom: false,
         });
 
-        map.current.on("load", () => {
-            /* Add the data to your map as a layer */
-            map.current.addLayer({
-                id: "locations",
-                type: "circle",
-                /* Add a GeoJSON source containing place coordinates and information. */
-                source: {
-                    type: "geojson",
-                    data: stores,
-                },
-            });
-        });
-
+        // Add controls
         map.current.addControl(new mapboxgl.FullscreenControl(), "top-left");
         map.current.addControl(new mapboxgl.NavigationControl(), "top-left");
         map.current.addControl(
@@ -58,23 +56,38 @@ export default function Home() {
         return () => {
             map.current.remove();
         };
-    }, []);
+    }, [TOKEN, mapId, theme]);
+
+    useEffect(() => {
+        if (!data || isPending || isFetching) return;
+
+        data.forEach((post) => {
+            const { latitude, longitude, title } = post;
+            const marker = new mapboxgl.Marker()
+                .setLngLat([longitude, latitude])
+                .addTo(map.current);
+
+            marker.getElement().addEventListener("click", () => {
+                alert(`Marker clicked: ${title}`);
+            });
+        });
+    }, [data, isPending, isFetching, theme]);
+
+    useEffect(() => {
+        if (map.current) {
+            const newMapStyle = mapId
+                ? `mapbox://styles/mapbox/${mapId}`
+                : theme === "dark"
+                ? "mapbox://styles/mapbox/dark-v11"
+                : "mapbox://styles/mapbox/light-v11";
+            map.current.setStyle(newMapStyle);
+        }
+    }, [theme, mapId]);
 
     return (
-        <Suspense
-            fallback={
-                <h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Laborum, fugiat ab dignissimos iure culpa voluptas! Mollitia
-                    eius molestiae non autem ducimus necessitatibus odit,
-                    ratione maxime optio nulla saepe debitis inventore!
-                </h1>
-            }
-        >
-            <Section>
-                <MapSheet />
-                <div className="h-screen w-full" ref={mapContainer} />
-            </Section>
-        </Suspense>
+        <Section>
+            <MapSheet />
+            <div className="h-screen w-full" ref={mapContainer} />
+        </Section>
     );
 }
--- components/MapSheet.tsx
+++ components/MapSheet.tsx
@@ -1,36 +1,71 @@
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet";
 import { Button } from "./ui/button";
 import { SlidersHorizontal } from "lucide-react";
 import { Slider } from "./ui/slider";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Drawer, DrawerContent, DrawerTrigger } from "./ui/drawer";
+import { useRouter } from "next/navigation";
 
 export default function MapSheet() {
+    const router = useRouter();
     return (
         <div className="fixed bottom-16 mx-2 z-50 bg-background mt-5 w-fit rounded-full flex justify-between gap-5 p-4 border">
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button size="icon" variant="secondary">
+            <Drawer>
+                <DrawerTrigger asChild>
+                    <Button size="icon" variant="outline">
                         <SlidersHorizontal />
                     </Button>
-                </SheetTrigger>
-                <SheetContent>
-                    <SheetHeader>
-                        <SheetTitle>Are you absolutely sure?</SheetTitle>
-                        <SheetDescription>
-                            This action cannot be undone. This will permanently
-                            delete your account and remove your data from our
-                            servers.
-                        </SheetDescription>
-                    </SheetHeader>
-                </SheetContent>
-            </Sheet>
+                </DrawerTrigger>
+                <DrawerContent className="space-y-5 h-[50vh] max-w-md mx-auto flex flex-col items-center">
+                    <h3 className="text-center">Map Type</h3>
+                    <div className="flex gap-5">
+                        <div
+                            onClick={() => router.push("/map")}
+                            className="rounded-lg overflow-hidden flex flex-col items-center"
+                        >
+                            <div className="aspect-video relative h-12">
+                                <Image
+                                    objectFit="cover"
+                                    src="/icons/standard.png"
+                                    fill
+                                    alt="image"
+                                />
+                            </div>
+                            Standard
+                        </div>
+
+                        <div
+                            onClick={() => router.push("/map?id=satellite-v9")}
+                            className="rounded-lg overflow-hidden flex flex-col items-center"
+                        >
+                            <div className="aspect-video relative h-12">
+                                <Image
+                                    objectFit="cover"
+                                    src="/icons/satellite.png"
+                                    fill
+                                    alt="image"
+                                />
+                            </div>
+                            Satellite
+                        </div>
+
+                        <div
+                            onClick={() => router.push("/map?id=streets-v12")}
+                            className="rounded-lg overflow-hidden flex flex-col items-center"
+                        >
+                            <div className="aspect-video relative h-12">
+                                <Image
+                                    objectFit="cover"
+                                    src="/icons/street.png"
+                                    fill
+                                    alt="image"
+                                />
+                            </div>
+                            Street
+                        </div>
+                    </div>
+                </DrawerContent>
+            </Drawer>
             <div className="flex items-center gap-2">
                 <span>₱0</span>
                 <Slider
